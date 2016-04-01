fd7e356588028195795f87eb735e4bc9f38034ef
Daniel Appelt <daniel.appelt@gmail.com>
2016-04-01 16:11:00 +0200

    Finish events block in multisite_front_page
    - Title and "more" link target page "veranstaltungen" which is created
      automatically by Events Manager plugin.
    - Optimize the introduction using a corresponding logic.
    - Remove some commented HTML which is now created from PHP.

--- multisite_front_page.php
+++ multisite_front_page.php
@@ -20,48 +20,28 @@ get_header(); ?>
 	<div class="welcome-text">
         <h1><?php echo get_bloginfo(); ?></h1>
         <p class="entry-text"><?php echo get_option('blog_long_description'); ?></p>
-<!--
-		<h1>Ermekeilinitiative <span style="text-transform:none;">e. V.</span></h1>
-		<p class="entry-text">Das Quartier mit dem integrativem Nutzungskonzept – entwickelt von und für BürgerInnen.</p>
--->
 		<p class="text-center">
 		<a href="#" class="button white-ghost">Mehr erfahren</a>
 		</p>
 	</div>
 </div>
 
+<?php ### Introduction: based on a (separate) page 'initiative' ?>
 <div class="row section">
 	<div class="small-12 columns text-center">
-        <?php
-        // See https://developer.wordpress.org/reference/functions/the_content/
-        $args = array(
-            'name'           => 'initiative',
-            'post_type'      => 'page',
-            'post_status'    => 'publish',
-            'posts_per_page' => 1
-        );
+<?php
+    $page = get_page_by_path('initiative');
 
-        $posts = get_posts( $args );
-        
-        if( $posts ) {
-            $post = $posts[0];
-            setup_postdata( $post );
-        ?> 
-            <h2 class="dark-green"><?php the_title(); ?></h2>
-            <?php the_content(''); ?>
-        <?php
-        }
+    if( $page ) {
+?>
+        <h2 class="dark-green"><?php echo get_the_title($page); ?></h2>
+<?php
+        // See https://developer.wordpress.org/reference/functions/the_content/
+        setup_postdata( $page );
+        the_content('');
         wp_reset_postdata();
-        ?>
-<!--
-		<h2 class="dark-green">Die Ermekeilinitiative e. V.</h2>
-		<p>
-		Das Ermekeil-Karree: Inmitten der Bonner Südstadt, umgeben von Wohnhäusern, Geschäften, Cafés und Gaststätten. Die historischen Gebäude befinden sich mit weiteren neueren Bauten auf einem weitläufigen Gelände. Dieses wird begrenzt von den Straßen Bonner Talweg, Reuterstraße, Argelanderstraße und Ermekeilstraße und umfasst eine Gesamtfläche von insgesamt 25.000 qm - das entspricht der Fläche von drei bis vier Fußballfeldern.
-		</p>
-		<p>
-		Unser Verein zur zivilen Nutzung der Ermekeilkaserne: „Ermekeilinitiative e. V.“, setzt sich seit 2005 aktiv für eine neue Gestaltung des Geländes als sozial, kulturell und nach Generationen gemischtes Quartier mit vielfältigen und ökologisch nachhaltigen Nutzungen ein. Hierfür hat die Initiative ein integratives Nutzungskonzept entwickelt und lädt alle BürgerInnen und AnwohnerInnen ein sich an der Schaffung eines solchen Ortes der Vielfalt zu beteiligen.
-		</p>
--->
+    }
+?>
 		<p style="margin-top: 2rem;">
 		<a href="initiative#ziele" class="button green-ghost">Unsere Ziele</a>
 		<a href="#" class="button blue" style="margin-left: 2rem;">Mitglied werden</a>
@@ -69,6 +49,7 @@ get_header(); ?>
 	</div>
 </div>
 
+<?php ### Themen und Arbeitsgruppen: based on network blogs (and pages?) ?>
 <div class="row expanded section working-groups" id="arbeitsgruppen">
 	<div class="small-up-1 medium-up-2 large-up-3" style="max-width: 990px; margin: 0 auto;">
 		<h2 class="brown">Themen- und Arbeitsgruppen der Initiative</h2>
@@ -107,7 +88,8 @@ get_header(); ?>
 #       Return to network's main blog.
         switch_to_blog($network_id);
         ?>
-        <!-- TODO: how to handle Themengruppen und Repair Café -->
+
+        <?php # TODO: how to handle Themengruppen und Repair Café ?>
 		<div class="column">
 			<div class="card">
 				<div class="image-content">
@@ -156,14 +138,16 @@ get_header(); ?>
 -->
 	</div>
 </div>
+
+<?php ### Veranstaltungen: based on Events Manager Plugin http://wp-events-plugin.com/ ?>
 <div class="row section" id="termine">
 	<div class="small-up-1 medium-up-2 large-up-3" style="max-width: 990px; margin: 0 auto;">
-		<h2 class="blue">Die nächsten Termine und Veranstaltungen</h2>
-
+		<h2 class="blue">
 <?php
-#echo EM_Locations::output(array());
-#print_r(EM_Events::get_default_search($args));
-
+    echo get_the_title(get_page_by_path('veranstaltungen'));
+?>
+		</h2>
+<?php
 $args = array(
     'category' => '-7',  # Exclude category garden. TODO: this should only exlcude Gartenöffnungszeiten and be customizable!
     'limit' => 10,
@@ -172,7 +156,7 @@ $args = array(
 			<a href="#_EVENTURL" class="url">
 				<div class="schedule-card">
 					<div class="calendar-sheet">
-						<time class="dtstart" datetime="#@c">										<!-- INSERT DATE HERE -->
+						<time class="dtstart" datetime="#@Y-#@m-#@d">
                             <span class="month">#@M.</span>
 							<span class="day">#@d</span>
 						</time>
@@ -188,29 +172,10 @@ $args = array(
 
 echo EM_Events::output($args);
 ?>
-<?php
-/*		<div class="column vevent">
-			<a href="#" class="url">																		<!-- INSERT SCHEDULE-URL HERE -->
-				<div class="schedule-card">
-					<div class="calendar-sheet">
-						<time class="dtstart" datetime="YYYY-MM-DD">										<!-- INSERT DATE HERE -->
-							<span class="month">Jan.</span>													<!-- INSERT SHORT MONTH (GERMAN) HERE -->
-							<span class="day">04</span>														<!-- INSERT SHORT DAY (GERMAN) HERE -->
-						</time>	
-					</div>
-					<div class="schedule-text">
-						<span class="schedule-location location">Haus 2 – Raum 107</span>						<!-- INSERT LOCATION HERE -->
-						<span class="schedule-title summary">Treffen der AG Quartier und Kultur</span>		<!-- INSERT SCHEDULE-TITLE HERE -->
-					</div>
-				</div>
-			</a>
-		</div>
-*/     
-?>
 	</div>
 	<div class="columns end">
 		<p class="text-center">
-		    <a href="#" class="button blue-ghost">Alle Termine anzeigen</a>
+		    <a href="veranstaltungen/" class="button blue-ghost">Alle Termine anzeigen</a>
 		</p>
 	</div>
 </div>
